09309fb2fdf97c4a7a70affaa565ac9571489cd7
Lucas Azevedo <lhs_azevedo@hotmail.com>
2024-09-09 23:07:32 -0300

    Fix LessonList Story

--- src/components/LessonList/LessonList.stories.tsx
+++ src/components/LessonList/LessonList.stories.tsx
@@ -10,8 +10,94 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const SingleUnit: Story = {
   args: {
-    // Add your props here
+    units: [
+      {
+        id: 1,
+        order: 0,
+        title: "Unit 1",
+        unitProgress: [],
+        lessons: [
+          {
+            id: 1,
+            title: "Lesson 1",
+            description: "Description 1",
+            challenges: "",
+            unitId: 1,
+            order: 0,
+            lessonProgress: [],
+          },
+          {
+            id: 2,
+            title: "Lesson 2",
+            description: "Description 2",
+            challenges: "",
+            unitId: 1,
+            order: 1,
+            lessonProgress: [],
+          },
+        ],
+      },
+    ]
+  },
+};
+
+export const MultiUnit: Story = {
+  args: {
+    units: [
+      {
+        id: 1,
+        order: 0,
+        title: "Unit 1",
+        unitProgress: [],
+        lessons: [
+          {
+            id: 1,
+            title: "Lesson 1",
+            description: "Description 1",
+            challenges: "",
+            unitId: 1,
+            order: 0,
+            lessonProgress: [],
+          },
+          {
+            id: 2,
+            title: "Lesson 2",
+            description: "Description 2",
+            challenges: "",
+            unitId: 1,
+            order: 1,
+            lessonProgress: [],
+          },
+        ],
+      },
+      {
+        id: 2,
+        order: 1,
+        title: "Unit 2",
+        unitProgress: [],
+        lessons: [
+          {
+            id: 3,
+            title: "Lesson 3",
+            description: "Description 3",
+            challenges: "",
+            unitId: 2,
+            order: 0,
+            lessonProgress: [],
+          },
+          {
+            id: 4,
+            title: "Lesson 4",
+            description: "Description 4",
+            challenges: "",
+            unitId: 2,
+            order: 1,
+            lessonProgress: [],
+          },
+        ],
+      },
+    ]
   },
 };
